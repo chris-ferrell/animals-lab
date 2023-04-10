3a4e9bdbc04e6b5bc5db603726ee8b03904a4072
unknown-bytes <unknown.develops@gmail.com>
2023-04-10 17:21:25 -0400

    edit/update animal working

--- controller/animals.js
+++ controller/animals.js
@@ -4,11 +4,27 @@ const animals = require('../models/animals.js');
 
 // routes {
     // Post Routes
-    
+    router.post('/', async (req, res) => {
+        console.log(req.body)
+        req.body.extinct = req.body.extinct === 'on' ? true : false;
+        res.redirect('/animals');
+    })
+    // Update
+    router.put('/:id', async (req, res) =>  {
+        const id = req.params.id;
+        req.body.extinct = req.body.extinct === 'on' ? true : false;
+        const Animals = await animals.findByIdAndUpdate(id, req.body, {
+            new: true,
+        });
+        res.redirect('/animals');
+    })
     // New
 
     // Edit
-
+    router.get('/:id/edit', async (req, res) => {
+        const animal = await animals.findById(req.params.id);
+        res.render("animals/edit.ejs", {animal});
+    })
     // Show
     router.get('/', async (req,res) => {
         const Animals = await animals.find({});
@@ -19,6 +35,11 @@ const animals = require('../models/animals.js');
                 
             });
     });
-
+    // Show one animal
+    router.get('/:id', async (req, res) => {
+        const animal = await animals.findById(req.params.id);
+        // res.send(animal);
+        res.render("animals/show.ejs", {animal})
+    });    
 // }
 module.exports = router;
